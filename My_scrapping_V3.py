92dd5041f4ebd2afc6bfa70e3e7d16dc25a8d3da
Karthik8396 <54445644+Karthik8396@users.noreply.github.com>
2019-09-15 19:50:45 +0530

    FeedParser Scrapping

--- My_scrapping_V3.py
+++ My_scrapping_V3.py
@@ -1,4 +1,4 @@
-from builtins import print, enumerate
+
 
 import bs4 as bs
 from urllib import request
@@ -10,6 +10,9 @@ import feedparser
 #start_time = time.time()
 ##TO DO GO TO PAGE ADD ?page=2 in the URL to continue downloading the pages .You will get the new RSS Feed XML with new contents
 
+#SCRAPPING IS DONE BY RSS FEED
+# FEEDPARSER MODULE IS USED TO PARSE THE XML RSS
+
 
 # count=3  #limiting the number of downloads
 # flag=0
@@ -19,11 +22,11 @@ req = http.request('GET','https://old.reddit.com/r/all/.rss',headers = { 'User-A
 # #setting the header to make the site think the request came from the browser
 sauce=req.data.decode('utf-8')   #decoding the response
 
-fd=feedparser.parse(sauce)
+fd=feedparser.parse(sauce)  #parsing the xml file 
 
 
 req.release_conn()
-entry=fd.get('entries')
+entry=fd.get('entries')  #json file is retured after parsing and values can be accessed by get function
 #print(entry)
 for i in entry:
     html = bs.BeautifulSoup(i.content[0].value, 'html.parser')
@@ -32,5 +35,5 @@ for i in entry:
 
         if i.text=='[link]' and 'old.reddit.com' not in i.get('href') :
             image_links.append(i.get('href'))
-            print(i.get('href'))
-print(len(image_links))
+            print(i.get('href'))                                            #getting all content links from the reddit page
+print(len(image_links))                                                     
